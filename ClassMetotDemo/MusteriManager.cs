5c16fb9ff73c0973c8b95a4213cfb5f098619212
has ozg <hasan_ozgen@windowslive.com>
2021-01-18 23:01:29 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri+ " eklendi");
+        }
+
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri + " listelendi");
+
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri + " silindi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Id = 1;
+            musteri1.Ad = "Hasan";
+            musteri1.Soyad = "Özgen";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkleme(musteri1);
+            musteriManager.MusteriListeleme(musteri1);
+            musteriManager.MusteriSilme(musteri1);
+        }
+    }
+}
